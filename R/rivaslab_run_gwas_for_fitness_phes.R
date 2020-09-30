27f287a08a23258f37e4db9d4e0a14f3430abce8
david-dd-amar <ddam.am@gmail.com>
2020-09-29 18:03:43 -0700

    Update rivaslab_run_gwas_for_fitness_phes.R

--- R/rivaslab_run_gwas_for_fitness_phes.R
+++ R/rivaslab_run_gwas_for_fitness_phes.R
@@ -1,22 +1,14 @@
+library(data.table)
 
 out_path = "/oak/stanford/groups/euan/projects/fitness_genetics/ukbb/gwas/"
 
 rivaslab_pheno_codes_file = "/home/users/davidama/repos/ukbb-tools/05_gbe/phenotype_info.tsv"
 rivaslab_codes = read.delim(rivaslab_pheno_codes_file,stringsAsFactors = F,header=T)
 rownames(rivaslab_codes) = rivaslab_codes[,1]
-activity_phenotypes = rivaslab_codes[
-  grepl("activity",rivaslab_codes[,2],ignore.case = T) & 
-    !grepl("anxi",rivaslab_codes[,2],ignore.case = T) &
-    !grepl("depression",rivaslab_codes[,2],ignore.case = T),1]
-activity_phenotypes = union(activity_phenotypes,
-                            c(  "VigActivityN" = "INI1003040",
-                                "ModActivityN" = "INI1003039"))
-tested_phenotypes = c(
-  "Exercise_HR" = "INI20005983",
-  "MaxWD" = "INI30005983",
-  "Recovery" = "INI40005983",
-  "RestingHR" = "INI102"
-)
+
+# define the self-reported activity phenotype
+activity_phenotypes = c("INI884","INI894","INI904","INI914")
+exercise_phenotypes = c("RestingHR" = "INI102")
 
 ethnicity_path = "/oak/stanford/groups/mrivas/private_data/ukbb/24983/sqc/population_stratification/"
 ethnicity = "white_british.phe"
@@ -24,10 +16,60 @@ eth_files = list.files(ethnicity_path)
 eth_file = eth_files[grepl(ethnicity,eth_files)]
 eth_file = paste(ethnicity_path,eth_file,sep='')
 # Get the ethnicities - subject subset to use in the current analysis
-eth_subjects = read.delim(eth_file,header=F,stringsAsFactors = F)
+eth_subjects = fread(eth_file,header=F,stringsAsFactors = F,data.table=F)
 eth_subjects = as.character(eth_subjects[,1])
 print("loaded current ethnicity subjects, number of subject ids is:")
 print(length(eth_subjects))
+
+# Read the pheno
+master_phe_path = "/oak/stanford/groups/mrivas/private_data/ukbb/24983/phewas/resources/master.phe"
+phe = fread(master_phe_path,header = T,stringsAsFactors = F,data.table = F)
+rownames(phe) = as.character(phe[,1])
+phe = phe[intersect(eth_subjects,rownames(phe)),]
+print("Dim of master phe")
+print(dim(phe))
+activity_phenotypes = activity_phenotypes[activity_phenotypes %in% colnames(phe)]
+exercise_phenotypes = exercise_phenotypes[exercise_phenotypes %in% colnames(phe)]
+
+# PCA of the physical activity scores
+act_phe = phe[,activity_phenotypes]
+act_phe = act_phe[apply(act_phe>0,1,all),]
+act_pca = prcomp(act_phe,retx = T)
+vars = act_pca$sdev^2
+cumsum(vars/sum(vars)) # 79% of the variability in PC1 - Sept 2020
+# corr between PC1 and the other scores
+pc1_corrs = c(cor(act_pca$x[,1],act_phe)) # negative correlation with reported activity - Sept 2020
+names(pc1_corrs) = rivaslab_codes[colnames(act_phe),2]
+# make sure we are positively correlated with activity
+PC1 = act_pca$x[,1]
+if(sum(pc1_corrs<0)/length(pc1_corrs) >0.5){PC1 = -PC1}
+
+# add the resting HR
+restHR = phe[,"INI102"]
+names(restHR) = rownames(phe)
+restHR = restHR[restHR > 0]
+# intersect
+shared = intersect(names(restHR),names(PC1))
+restHR = restHR[shared]
+PC1 = PC1[shared]
+# Sept 2020:
+# > cor(restHR,PC1,method="spearman")
+#[1] -0.033 (mild but sig)
+
+# high values mean high activity
+PC1_zs = (PC1 - mean(PC1))/sd(PC1)
+# high values mean "high fitness"
+restHR_zs = (restHR - mean(restHR))/sd(restHR)
+restHR_zs = -1*restHR_zs
+# Get the diff
+activity_minus_resthr = PC1_zs - restHR_zs
+
+# print the final phe file
+curr_pcs = paste("PC",1:10,sep="")
+covariate_names = c("#FID","IID","sex","age","Array",curr_pcs)
+all_covs = cbind(phe[names(activity_minus_resthr),covariate_names],activity_minus_resthr)
+write.table(all_covs,file=paste(out_path,"activity_minus_resthr.phe",sep=""),
+                sep=" ",row.names = F,col.names = T,quote = F)
 
 # Genetics information in a bed file
 pgen_path = "/oak/stanford/groups/mrivas/private_data/ukbb/24983/imp/pgen/"
@@ -35,102 +77,13 @@ all_bed_files = list.files(pgen_path)
 all_bed_files = all_bed_files[grepl("bed$",all_bed_files)]  
 all_bed_files = paste(pgen_path,all_bed_files,sep="")
 
-# Read the pheno
-master_phe_path = "/oak/stanford/groups/mrivas/private_data/ukbb/24983/phewas/resources/master.phe"
-library(data.table,lib="~/R/packages/")
-phe = fread(master_phe_path,header = T,stringsAsFactors = F,data.table = F)
-tested_phenotypes[is.element(tested_phenotypes,set=colnames(phe))]
-rownames(phe) = as.character(phe[,1])
-phe = phe[intersect(eth_subjects,rownames(phe)),]
-print("Dim of master phe")
-print(dim(phe))
 
-# Add missing phenotypes to the phe table
-additional_phe_paths = "/oak/stanford/groups/mrivas/ukbb/24983/phenotypedata/extras/physical_activity/phe/"
-additional_phe_files = list.files(additional_phe_paths)
-additional_phe = c()
-for(phe_code in c(activity_phenotypes,tested_phenotypes)){
-  currfile = additional_phe_files[grepl(phe_code,additional_phe_files)]
-  if(length(currfile)==0){next}
-  print(paste(phe_code,"was found in the extras dir"))
-  curr_v = fread(paste(additional_phe_paths,currfile,sep=""),data.table = F,header = F,stringsAsFactors = F)
-  rownames(curr_v) = as.character(curr_v[,1])
-  newv = rep(NA,nrow(phe))
-  names(newv) = rownames(phe)
-  currnames = intersect(rownames(curr_v),rownames(phe))
-  newv[currnames] = curr_v[currnames,3]
-  print(newv[currnames][1:10])
-  additional_phe = cbind(additional_phe,newv)
-  rownames(additional_phe) = names(newv)
-  colnames(additional_phe)[ncol(additional_phe)] = phe_code
-}
-additional_phe[additional_phe==-9] = NA
-phe = cbind(phe,additional_phe)
 
-# PCA of the physical activity scores
-act_phe = phe[,intersect(colnames(phe),activity_phenotypes)]
-act_pca = prcomp(act_phe,retx = T)
-vars = act_pca$sdev^2
-cumsum(vars/sum(vars)) # 78% of the variability in PC1
-# corr between PC1 and the other scores
-pc1_corrs = c(cor(act_pca$x[,1],act_phe)) # positive correlation
-names(pc1_corrs) = rivaslab_codes[colnames(act_phe),2]
-# make sure we are positively correlated with activity
-PC1 = act_pca$x[,1]
-if(sum(pc1_corrs<0)/length(pc1_corrs) >0.5){PC1 = -PC1}
 
-ExHR = additional_phe[,tested_phenotypes[1]]
-rHR = phe[,tested_phenotypes[4]]
-normExHR = ExHR^2 / rHR
-scoresMat = cbind(ExHR,rHR,normExHR,PC1)
-scoresMat = scoresMat[!apply(is.na(scoresMat),1,any),]
-cor(scoresMat)
 
-tb = table(normExHR < quantile(normExHR,na.rm = T,probs = 0.1),
-      PC1 < quantile(PC1,na.rm = T,probs = 0.1))
-# tb = table(ExHR > quantile(ExHR,na.rm = T,probs = 0.9),
-#            rHR > quantile(rHR,na.rm = T,probs = 0.9))
-fisher.test(tb)
 
-# Create folders with a phe file and a covariate file
-PCs = c(5,10)
-for(npc in PCs){
-  curr_pcs = paste("PC",1:npc,sep="")
-  curr_covariate_names = c("sex","age","Array",curr_pcs)
-  for(pheno_name in names(tested_phenotypes)){
-    pheno_code = tested_phenotypes[pheno_name]
-    if(!is.element(pheno_code,set=colnames(phe))){next}
-    curr_path = paste(out_path,pheno_name,"_PCs",npc,"/",sep="")
-    try(system(paste("mkdir",curr_path)))
-    # create the phe file and the covars file
-    y = phe[,pheno_code]
-    x = phe[,curr_covariate_names]
-    x = as.matrix(x)
-    rownames(x) = rownames(phe)
-    names(y) = rownames(phe)
-    # If the phenotype is numeric, regress out the covariats
-    newy = y
-    covs = NULL
-    if(mode(y)=="numeric" && length(unique(y))>2){
-      lm = lm(y~x)
-      residuals = lm$residuals
-      newy = residuals
-    }
-    else{
-      covs = cbind(rownames(phe),rownames(phe),x)
-      colnames(covs) = c("#FID","IID",curr_covariate_names)
-    }
-    newy = cbind(names(newy),names(newy),newy)
-    colnames(newy) = c("#FID","IID",pheno_name)
-    write.table(newy,file=paste(curr_path,pheno_name,".phe",sep=""),
-                sep=" ",row.names = F,col.names = T,quote = F)
-    if(! is.null(covs)){
-      write.table(covs,file=paste(curr_path,"covs.txt",sep=""),
-                  sep=" ",row.names = F,col.names = T,quote = F)
-    }
-  }
-}
 
+# Ignore the rest of the file for now (Sept 2020)
 ###############################################################################
 ###############################################################################
 ###############################################################################

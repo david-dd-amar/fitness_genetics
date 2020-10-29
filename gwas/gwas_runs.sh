85a9670238a3a769ade5d68ca9321a3b5e5d5bcf
david-dd-amar <ddam.am@gmail.com>
2020-10-29 16:04:32 -0700

    Update gwas_runs.sh
    do not use the --remove flag
    add columns to the plink output

--- gwas/gwas_runs.sh
+++ gwas/gwas_runs.sh
@@ -1,18 +1,23 @@
 bfile=/oak/stanford/groups/euan/projects/fitness_genetics/analysis/mega_all_batches/merged_mega_data
 pheno=/oak/stanford/groups/euan/projects/fitness_genetics/pheno/master_phe_mega.phe
 pcs="EU_PC1,EU_PC2,EU_PC3,EU_PC4,EU_PC5"
-pop=/oak/stanford/groups/euan/projects/fitness_genetics/pheno/eu_cluster1.phe
-maf=0.05
-excl=/oak/stanford/groups/euan/projects/fitness_genetics/pheno/rel_to_remove.phe
+maf=0.01
+excl1=/oak/stanford/groups/euan/projects/fitness_genetics/pheno/rel_to_remove.phe
+excl2=/oak/stanford/groups/euan/projects/fitness_genetics/pheno/eu_pca_outliers.phe
 vexcl=/oak/stanford/groups/euan/projects/fitness_genetics/analysis/all_bad_snps.txt
+cols_add=cols=+ax,+a1countcc,+gcountcc,+a1freq,+a1freqcc,+a1count,+totallele
+
+excl="tmp_all_samps_to_rem.txt"
+less ${excl1} > ${excl}
+less ${excl2} >> ${excl}
 
 ml load plink2
 
 plink2 --bfile ${bfile} --chr 1-22 \
 	--pheno ${pheno} --pheno-name Treadmill.time.sec \
 	--covar ${pheno} --covar-name sex,${pcs} \
-	--keep ${pop} --remove ${excl} \
-	--glm skip hide-covar --maf ${maf} --adjust \
+	--remove ${excl} \
+	--glm skip hide-covar ${cols_add} --maf ${maf} --adjust \
 	--exclude ${vexcl} \
 	--require-covar --require-pheno \
 	--out cooper
@@ -20,26 +25,26 @@ plink2 --bfile ${bfile} --chr 1-22 \
 plink2 --bfile ${bfile} --chr 1-22 \
         --pheno ${pheno} --pheno-name VO2max..ml.kg.min. \
         --covar ${pheno} --covar-name sex,${pcs} \
-        --keep ${pop}  --remove ${excl} \
+        --remove ${excl} \
 	--exclude ${vexcl} \
-        --glm skip hide-covar --maf ${maf} --adjust \
+        --glm skip hide-covar ${cols_add} --maf ${maf} --adjust \
         --require-covar --require-pheno \
         --out elite
 
 plink2 --bfile ${bfile} --chr 1-22 \
         --pheno ${pheno} --pheno-name cooper_vs_elite \
         --covar ${pheno} --covar-name sex,${pcs} \
-        --keep ${pop}  --remove ${excl} \
+        --remove ${excl} \
         --exclude ${vexcl} \
-	--logistic skip hide-covar --maf ${maf} --adjust \
+	--logistic skip hide-covar ${cols_add} --maf ${maf} --adjust \
         --require-covar --require-pheno \
         --out cooper
 
 plink2 --bfile ${bfile} --chr 1-22 \
         --pheno ${pheno} --pheno-name cooper_vs_gp \
         --covar ${pheno} --covar-name sex,${pcs} \
-        --keep ${pop}  --remove ${excl} \
-        --logistic skip hide-covar --maf ${maf} --adjust \
+        --remove ${excl} \
+        --logistic skip hide-covar ${cols_add} --maf ${maf} --adjust \
         --exclude ${vexcl} \
 	--require-covar --require-pheno \
         --out cooper
@@ -47,8 +52,8 @@ plink2 --bfile ${bfile} --chr 1-22 \
 plink2 --bfile ${bfile} --chr 1-22 \
         --pheno ${pheno} --pheno-name elite_vs_gp \
         --covar ${pheno} --covar-name sex,${pcs} \
-        --keep ${pop}  --remove ${excl} \
+        --remove ${excl} \
         --exclude ${vexcl} \
-	--logistic skip hide-covar --maf ${maf} --adjust \
+	--logistic skip hide-covar ${cols_add} --maf ${maf} --adjust \
         --require-covar --require-pheno \
         --out elite
